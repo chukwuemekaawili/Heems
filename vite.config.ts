add6833c916ee787764cc5c764338c121bf8c858
chukwuemekaawili <chukwuemekaawili@gmail.com>
2026-02-22 04:12:51 +0100

    Fix: use absolute base path for production deployment

--- vite.config.ts
+++ vite.config.ts
@@ -5,10 +5,11 @@ import path from "path";
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   server: {
-    host: "::",
+    host: "0.0.0.0",
     port: 8080,
   },
   plugins: [react()],
+  base: "/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
